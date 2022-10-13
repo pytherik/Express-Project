7b555488e8acb2f9e9419e929ea877cd65e0c06d
pytherik <erikberndt@gmx.net>
2022-10-13 14:17:36 +0200

    append rudimental middleware

--- server.js
+++ server.js
@@ -16,22 +16,27 @@ const friends = [
 
 ];
 
+app.use((req, res, next) => {                    //* middleware - placed between request and response
+  const start = Date.now();
+  next();                                        //* dont't forget! otherwise the app gets stuck.
+  console.log(`${req.method} ${req.url} ${Date.now() - start}ms`);  //* time passing
+});
+
 app.get('/friends', (req, res) => {                  //* status 200, content-type: text/html
   res.json(friends);
 });
 
 app.get('/friends/:friendId', (req, res) => {     //* handle incoming id-request
-  const friendId = req.params.friendId;
+  const friendId = Number(req.params.friendId);   //* converting to number not necessary
   const friend = friends[friendId];
   if (friend) {
-    res.status(200).json(friend);                 //* set status manually
+    res.status(200).json(friend);                //* set status manually
   } else {
     res.status(404).json({                        //* set status manually
      error: "Freund*in existiert nicht."
     });
   }
 })
-
 
 
 app.listen(PORT, () => console.log(`Server is lisening on port ${PORT}`));
