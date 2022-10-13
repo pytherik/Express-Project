7c29bbf739e0b7fb3e7206eb88cc7f933c463595
pytherik <erikberndt@gmx.net>
2022-10-13 21:12:55 +0200

    add layout.hbs and  messages.hbs

--- controllers/messages.controller.js
+++ controllers/messages.controller.js
@@ -1,8 +1,13 @@
 const path = require('path');
 
 function getMessages(req, res) {            //* use named functions to enable node help debugging
-  
-  res.sendFile(path.join(__dirname, '..', 'public', 'images', 'Andere0012.jpg'));
+ 
+  res.render('messages', {
+    title: 'Nachrichten für meine Freunde',
+    friend: 'Hulk Hogan'
+  })
+ 
+  // res.sendFile(path.join(__dirname, '..', 'public', 'images', 'Andere0012.jpg'));
   // res.send(`<body style="background-color: #777;">
   //             <h1 style="font-size: 65;text-align: center; color: #f9d4a3;margin-top: 10%">
   //             Hallo Doofmann!
