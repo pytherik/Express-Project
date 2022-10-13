e480998e7379503929b6190859ae962a011bd7e9
pytherik <erikberndt@gmx.net>
2022-10-13 11:32:14 +0200

    set up 3 routes (app.get, app.post)

--- server.js
+++ server.js
@@ -4,4 +4,22 @@ const app = express();
 
 const PORT = 3000;
 
+
+app.get('/', (req, res) => {
+  res.send(`<body style="background-color: #777;">
+              <h1 style="font-size: 65;text-align: center; color: #f9d4a3;">
+              Blödmann!
+              </h1>
+            </body>`);
+})
+
+app.get('/messages', (req, res) => {
+  res.send(`<body style="background-color: #777;">
+              <h1 style="font-size: 65;text-align: center; color: #f9d4a3;margin-top: 10%">
+              Hallo Doofmann!
+              </h1>
+            </body>`);
+})
+
+
 app.listen(PORT, () => console.log(`Server is lisening on port ${PORT}`));
