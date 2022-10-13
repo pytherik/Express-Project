2bba030229856171c4693deab086fe09cbe5e8b3
pytherik <erikberndt@gmx.net>
2022-10-13 20:54:12 +0200

    set path for static files to public, add handlebars(hbs) template engine, create index.hbs

--- controllers/messages.controller.js
+++ controllers/messages.controller.js
@@ -1,9 +1,13 @@
+const path = require('path');
+
 function getMessages(req, res) {            //* use named functions to enable node help debugging
-  res.send(`<body style="background-color: #777;">
-              <h1 style="font-size: 65;text-align: center; color: #f9d4a3;margin-top: 10%">
-              Hallo Doofmann!
-              </h1>
-            </body>`);
+  
+  res.sendFile(path.join(__dirname, '..', 'public', 'images', 'Andere0012.jpg'));
+  // res.send(`<body style="background-color: #777;">
+  //             <h1 style="font-size: 65;text-align: center; color: #f9d4a3;margin-top: 10%">
+  //             Hallo Doofmann!
+  //             </h1>
+  //           </body>`);
 };
 
 function postMessage(req, res) {
--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require('express');
+const path = require('path');
 
 const friendsRouter = require('./routes/friends.router');
 const messagesRouter = require('./routes/messages.router');
-const aboutRouter = require('./routes/about.router');
 
 const app = express();
 
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'views'));
 const PORT = 3000;
 
 app.use((req, res, next) => {      
@@ -13,13 +15,17 @@ app.use((req, res, next) => {
   next();                                       
   console.log(`${req.method} ${req.baseUrl}${req.url} ${Date.now() - start}ms`);  
 });
-
-app.use((express.json()));           
+app.use(express.static(path.join(__dirname, 'public')))
+app.use((express.json()));
+app.get('/', (req, res) => {
+  res.render('index', {
+    title: 'Express-Example',
+    caption: 'Tours, Atomic-Café, 1989'
+  });
+});
 
 app.use('/friends', friendsRouter);       //* mount friendsRouter on '/friends' path
 app.use('/messages', messagesRouter);     //* mount messagesRouter on '/messages' path
-
-app.use('/about', aboutRouter); //!
 
 
 app.listen(PORT, () => console.log(`Server is lisening on port ${PORT}`));
